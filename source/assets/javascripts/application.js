50345c698d884aa2b20364c0d93163e0552fa8e8
Eric Gardner <gardner.ec@gmail.com>
2015-08-05 18:04:38 -0700

    enable basic panel toggle

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,1 +1,24 @@
 //= require_tree .
+function rightPanelToggle () {
+  var left = $(".panel--left");
+  var right = $(".panel--right");
+  left.toggleClass("panel--collapse");
+  right.toggleClass("panel--expand");
+}
+
+function leftPanelToggle() {
+  var left = $(".panel--left");
+  var right = $(".panel--right");
+  left.toggleClass("panel--expand");
+  right.toggleClass("panel--collapse");
+}
+
+$(document).ready(function() {
+
+  $("#rightPanelToggle").click(function (event) {
+    rightPanelToggle();
+    event.preventDefault();
+  })
+
+
+});
