7feca9b71f126cb9e11fbd8eac89a979f177f4bf
Eric Gardner <gardner.ec@gmail.com>
2015-08-11 12:19:45 -0700

    Set min zoom level at 1 and remove unused code.

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,12 +1,8 @@
 //= require_tree .
 //= require turbolinks
-function initializeMap () {
-  L.mapbox.accessToken = 'pk.eyJ1IjoiZWdhcmRuZXIiLCJhIjoiN2IyMmRlMTc0YTAwMzRjYWVhMzI5ZGY1YmViMGVkZTEifQ._576KIFjJ0S_dRHcdM2BmQ';
-  L.mapbox.map('map', 'egardner.n1p8bjh1');
-}
 
 function initLeaflet(catalogueNumber, pixelWidth, pixelHeight, objectMaxZoom) {
-  var mapMinZoom = 2;
+  var mapMinZoom = 1;
   var mapMaxZoom = objectMaxZoom;
   var map = L.map('map', {
     maxZoom: mapMaxZoom,
@@ -22,39 +18,10 @@ function initLeaflet(catalogueNumber, pixelWidth, pixelHeight, objectMaxZoom) {
   map.fitBounds(mapBounds);
   L.tileLayer('../assets/tiles/' + catalogueNumber + '/{z}/{x}/{y}.png', {
     noWrap: true
-    //minZoom: mapMinZoom, maxZoom: mapMaxZoom,
-    //bounds: mapBounds,
-    //maxBoundsViscosity: 1.0
   }).addTo(map);
-
 
   return map;
 }
-
-
-// function initLeaflet() {
-//   var mapMinZoom = 2;
-//   var mapMaxZoom = 5;
-//   var map = L.map('map', {
-//     maxZoom: mapMaxZoom,
-//     minZoom: mapMinZoom,
-//     crs: L.CRS.Simple
-//   }).setView([0, 0], mapMaxZoom);
-//
-//   var mapBounds = new L.LatLngBounds(
-//       map.unproject([0, 8192], mapMaxZoom),
-//       map.unproject([6656, 0], mapMaxZoom));
-//
-//   map.fitBounds(mapBounds);
-//   L.tileLayer('../assets/tiles/1/{z}/{x}/{y}.png', {
-//     minZoom: mapMinZoom, maxZoom: mapMaxZoom,
-//     bounds: mapBounds,
-//     //maxBoundsViscosity: 1.0,
-//     //attribution: 'Rendered with <a href="http://www.maptiler.com/">MapTiler</a>',
-//     noWrap: true
-//   }).addTo(map);
-// }
-
 
 function rightPanelToggle () {
   var left = $(".panel--left");
@@ -73,13 +40,16 @@ function leftPanelToggle() {
 
 $(document).ready(function() {
 
+  // Initialize
   var catalogueNumber = $(".object__content").data("catalogue");
-  var pixelWidth = $(".object__content").data("dimensions-width");
-  var pixelHeight = $(".object__content").data("dimensions-height");
-  var objectMaxZoom = $(".object__content").data("dimensions-max-zoom");
+  var pixelWidth      = $(".object__content").data("dimensions-width");
+  var pixelHeight     = $(".object__content").data("dimensions-height");
+  var objectMaxZoom   = $(".object__content").data("dimensions-max-zoom");
+  var map             = initLeaflet(
+                          catalogueNumber, pixelWidth, pixelHeight, objectMaxZoom
+                        );
 
-  map = initLeaflet(catalogueNumber, pixelWidth, pixelHeight, objectMaxZoom);
-
+  // Set up event listeners
   $("#rightPanelToggle").click(function (event) {
     rightPanelToggle();
     setTimeout(map.invalidateSize.bind(map), 1000);
