781ec024039249a786a29d0ddc3d7d213c2ca807
Eric Gardner <gardner.ec@gmail.com>
2015-09-02 17:41:22 -0700

    Enable multiple image views in leaflet viewer
    Using the auto-generated img_index.yml file, Leaflet can now pull all
    available layers for a given image when generating a deep zoom map.
    Users can switch between layers via the layer control in lower left.
    
    Several other improvements to the map code have been made here as well,
    in terms of code organization.

--- source/assets/javascripts/map-functions.js
+++ source/assets/javascripts/map-functions.js
@@ -8,33 +8,69 @@
 // as well as standard map components. Code for interacting with leaflet
 // should live here.
 
+CONFIG = {
+  mapboxAccessToken: "pk.eyJ1IjoiZWdhcmRuZXIiLCJhIjoiN2IyMmRlMTc0YTAwMzRjYWVhMzI5ZGY1YmViMGVkZTEifQ._576KIFjJ0S_dRHcdM2BmQ",
+  mapboxTileURL: "https://api.mapbox.com/v4/egardner.n1p8bjh1/{z}/{x}/{y}.png?access_token=",
+  imageTileURL: "http://gettypubs.github.io/maptiles/",
+  mapID: "map",
+  coords: [40.51379915504413, 17.193603515625],
+  defaultZoom: 8,
+
+};
+
+// -----------------------------------------------------------------------------
+// deepZoomSetup
+//
+// This function pulls data out of the DOM and uses it to set up new map instances.
+// Map info lives in a non-visible .object-data div on each catalogue page.
+// This function calls the other setup functions and returns a leaflet map object.
+
+function deepZoomSetup() {
+  var options = {
+    catNum:      $(".object-data").data("catalogue"),
+    pixelWidth:  $(".object-data").data("dimensions-width"),
+    pixelHeight: $(".object-data").data("dimensions-height"),
+    maxZoom:     $(".object-data").data("dimensions-max-zoom"),
+    views:       $(".object-data").data("views").layers
+  };
+
+  var map = initDeepZoom(options);
+  return map;
+}
 
 // =============================================================================
-// Set up Leaflet.js for geographic map
-function initLeafletMap() {
-  accessToken = 'pk.eyJ1IjoiZWdhcmRuZXIiLCJhIjoiN2IyMmRlMTc0YTAwMzRjYWVhMzI5ZGY1YmViMGVkZTEifQ._576KIFjJ0S_dRHcdM2BmQ';
-  // Replace 'mapbox.streets' with your map id.
-  var mapboxTiles = L.tileLayer('https://api.mapbox.com/v4/egardner.n1p8bjh1/{z}/{x}/{y}.png?access_token=' + accessToken, {
+// initMap
+//
+// Creates a geographic map using Mapbox tiles.
+// Returns a Leaflet map object.
+
+function initMap() {
+  var mapboxTiles = L.tileLayer(CONFIG.mapboxTileURL + CONFIG.mapboxAccessToken, {
     attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
   });
 
-  var map = L.map('map', { attributionControl: false })
+  var map = L.map(CONFIG.mapID, { attributionControl: false })
     .addLayer(mapboxTiles)
-    .setView([40.51379915504413, 17.193603515625], 8);
+    .setView(CONFIG.coords, CONFIG.defaultZoom);
 
+  // geoJson placeholder is currently loaded from geojson.js
   L.geoJson(geojsonFeature).addTo(map);
-
-  L.easyButton('<i class="icon ion-android-expand"></i>', leftPanelToggle)
-    .addTo(map);
-
+  L.easyButton('<i class="icon ion-android-expand"></i>', leftPanelToggle).addTo(map);
   return map
 }
 
+// -----------------------------------------------------------------------------
+// initDeepZoom (options)
+//
+// Creates a non-geographic Leaflet map for deep-zoom images.
+// Expects an options object containing properties for:
+// maxZoom, catNum, pixelWidth, pixelHeight, and views (array)
+// Returns a Leaflet map object.
 
-// Set up Leaflet.js for deep zoom images
-function initLeafletDeepZoom(catalogueNumber, pixelWidth, pixelHeight, objectMaxZoom) {
-  var mapMinZoom = (objectMaxZoom - 3);
-  var mapMaxZoom = objectMaxZoom;
+function initDeepZoom(options) {
+  var mapMinZoom = (options.maxZoom - 3);
+  var mapMaxZoom = options.maxZoom;
+
   var map = L.map('map', {
     maxZoom: mapMaxZoom,
     minZoom: mapMinZoom,
@@ -43,44 +79,45 @@ function initLeafletDeepZoom(catalogueNumber, pixelWidth, pixelHeight, objectMax
   }).setView([0, 0], mapMaxZoom);
 
   var mapBounds = new L.LatLngBounds(
-      map.unproject([0, pixelHeight], mapMaxZoom),
-      map.unproject([pixelWidth, 0], mapMaxZoom));
-
+    map.unproject([0, options.pixelHeight], mapMaxZoom),
+    map.unproject([options.pixelWidth, 0], mapMaxZoom)
+  );
   map.fitBounds(mapBounds);
 
-  // Temporarily hard-code these to Github
-  // Pull them from CDN once this is up
-  L.tileLayer('http://gettypubs.github.io/maptiles/terracottas/'
-    + catalogueNumber + '/main/{z}/{x}/{y}.png', { noWrap: true })
-    .addTo(map);
+  // Add default layer to map
+  L.tileLayer(CONFIG.imageTileURL + "terracottas/"
+    + options.catNum + '/main/{z}/{x}/{y}.png', { noWrap: true }).addTo(map);
 
-  L.easyButton('<i class="icon ion-android-expand"></i>', leftPanelToggle)
-    .addTo(map);
+  // Set up alternate view layers
+  var baseMaps = {};
+  for (var i = 0; i < options.views.length; i++) {
+    var layerName = options.views[i].name + " view";
+    var layerPath = options.views[i].path;
+    baseMaps[layerName] = createTileLayer(layerPath);
+  }
 
-  // Use this procedure to place elements on a map
-  // see here:
-  // gis.stackexchange.com/questions/76059/how-to-project-pixel-coordinates-in-leaflet
-
-  // var coords = map.unproject([2500, 2500], mapMaxZoom);
-  // var marker = L.marker(coords).addTo(map);
-
+  // Add map controls
+  L.control.layers(baseMaps).addTo(map).setPosition("bottomleft");
+  L.easyButton('<i class="icon ion-android-expand"></i>', leftPanelToggle).addTo(map);
   return map;
 }
 
-// DOM-specific setup code (where to find object info)
-function deepZoomSetup(){
-  var catalogueNumber = $(".object-data").data("catalogue");
-  var pixelWidth      = $(".object-data").data("dimensions-width");
-  var pixelHeight     = $(".object-data").data("dimensions-height");
-  var objectMaxZoom   = $(".object-data").data("dimensions-max-zoom");
-  var map             = initLeafletDeepZoom(
-                          catalogueNumber, pixelWidth, pixelHeight, objectMaxZoom
-                        );
-  // Returns a map object for use by other functions
-  return map;
+// -----------------------------------------------------------------------------
+// createTileLayer (path)
+//
+// Returns a Leaflet tileLayer object pointing at the path provided, w/default options.
+
+function createTileLayer(path) {
+  return L.tileLayer(
+    'http://gettypubs.github.io/maptiles/' + path + '/{z}/{x}/{y}.png', {
+      noWrap: true,
+      attributionControl: false
+  });
 }
 
-// =============================================================================
+// -----------------------------------------------------------------------------
+// addMapResizeListener(map)
+//
 // Fix Leaflet cropping bug after dynamically re-sizing the map area by
 // calling invalidateSize() whenever map region changes. Requires an already-
 // initialized map object passed in as argument.
