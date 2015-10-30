f3f0bbe931bf5307bb78ad00f111192de06ea9a3
egardner <gardner.ec@gmail.com>
2015-10-30 16:10:11 -0700

    Basic Vue.js catalogue implementation
    At this point a working catalogue component exists as a full-featured
    Vue instance. The component contains a range of features from the
    framework, including custom filters, directives, and computed
    properties. 2-way data binding between the <select> elements on the page
    and the catalogue dataset removes the need for any jQuery-based handling
    of user input.
    
    One big advantage of Vue is how easy it makes defining various states in
    a template. In this example, a computed property called currentEntries
    calcultes the result from the currently selected filters. If the length
    of this result is zero, a v-if directive switches on with a message for
    the user.
    
    One current downside: v-model bindings only work on HTML form elements.
    Which means the custom dropdowns from the jQuery version of this page
    may be harder to re-create.
    
    One bit of remaining functionality to implement is the "clear filters"
    button - this should call a custom method that resets the `selection`
    object to its initial state.

--- source/assets/javascripts/catalogue.js
+++ source/assets/javascripts/catalogue.js
@@ -1,5 +1,7 @@
+//= require lib/underscore-min
 //= require lib/vue
 //= require lib/vue-resource.min
+
 
 Vue.config.debug = true;
 var catalogue = {
@@ -10,8 +12,63 @@ var catalogue = {
     loaded: false,
     baseUrl: "http://gettypubs.github.io/Terracottas/assets/images/",
     sortKey: '["info"]["cat"]',
+    selection: {
+      location:   "All regions",
+      typology:   "All types",
+      group:      "All groups",
+      date: {
+        start: "550",
+        end: "0",
+        text: "All dates"
+      }
+    },
+    locationOptions: [
+      "All regions",
+      "Taranto region",
+      "Canosa",
+      "Medma",
+      "South Italy",
+      "Sicily"
+    ],
+    typologyOptions: [
+      "All types",
+      "Statue",
+      "Head",
+      "Bust",
+      "Statuette",
+      "Plaque",
+      "Mask",
+      "Relief",
+      "Altar",
+      "Thymiaterion",
+      "Clipeus"
+    ],
+    groupOptions: [
+      "All groups",
+      "Group of a Seated Poet and Sirens",
+      "Group of Heads and Busts from the Taranto Region",
+      "Four Statues of Mourning Women from Canosa",
+      "Statuettes of a Seated Eros",
+      "Pair of Altars with the Myth of Adonis"
+    ],
+    dateOptions: [
+      {start: "550", end:"0",   text: "All dates"},
+      {start: "550", end:"500", text: "550–500 BC"},
+      {start: "499", end:"450", text: "499–450 BC"},
+      {start: "400", end:"350", text: "400–350 BC"},
+      {start: "399", end:"350", text: "399–350 BC"},
+      {start: "349", end:"300", text: "349–300 BC"},
+      {start: "299", end:"250", text: "299–250 BC"},
+      {start: "249", end:"200", text: "249–200 BC"},
+      {start: "199", end:"150", text: "199–150 BC"},
+      {start: "149", end:"100", text: "149–100 BC"}
+    ]
+  },
 
-    // current filter state (aka gridSelection object) lives here as well
+  computed: {
+    currentEntries: function() {
+      return this.$eval('entries | selectedItems ');
+    }
   },
 
   methods: {
@@ -31,16 +88,27 @@ var catalogue = {
   },
 
   filters: {
-    // various filters live here.
-    // Instead of getting args from template events, they should just filter
-    // against the value of this.filterState (or whatever)
-    // Template will use v-model for 2-way binding between selection elements
-    // and filterState data object.
-
-    // regionFilter
-    // typeFilter
-    // groupFilter
-    // dateFilter
+    selectedItems: function () {
+      var selection = this.selection;
+      return _.chain(this.entries)
+        .filter(function (entry) { // Location
+          if (selection.location == "All regions") { return entry; }
+          return entry.info.location == selection.location;
+        })
+        .filter(function (entry) { // Typology
+          if (selection.typology == "All types") { return entry; }
+          return entry.info.typology == selection.typology;
+        })
+        .filter(function (entry) { // Group
+          if (selection.group == "All groups") { return entry; }
+          return entry.info.group == selection.group;
+        })
+        .filter(function (entry) { // Date range
+          return entry.meta.start_date >= selection.date.end
+                 && entry.meta.end_date <= selection.date.start;
+        })
+        .value();
+    }
   },
 
   ready: function (argument) {
