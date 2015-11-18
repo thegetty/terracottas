731f868c6aafda38457d9ff4d6ff863b90e26547
Eric Gardner <gardner.ec@gmail.com>
2015-11-18 09:40:44 -0800

    Add PDF Output extension
    This commit adds a basic Middleman extension for PDF output. The basic
    process works as follows:
    - On `middleman build`, a new version of each catalogue page is created,
      using the `print-template.html` file. This is proxied to the
      `/print-catalogue/` directory.
    - The extension loops through all pages wiht a `sort_order` defined and
      creates a list of contents in the correct reading order: frontmatter,
      catalogue entries, backmatter. See the extension source for details.
    - This list is fed to the Prince CLI (the extension assumes it is
      available), which generates a single PDF file of the entire book. A
      print CSS stylesheet helps make the result look better.
    
    Future work: Right now the reliance on an external dependency (Prince)
    is not very well documented. Some way of checking/requiring that this is
    available would be useful to add. Also tests.
    
    This plugin could probably be easily adapted to work for Epub output as
    well (though in that case a set of template files ought to be included
    in the extension, so they do not clutter up the rest of the app).

---
+++ extensions/pdf.rb
@@ -0,0 +1,60 @@
+class PDF < Middleman::Extension
+  option :print_template
+  # option :output_path
+
+  def initialize(app, options_hash={}, &block)
+    super
+
+    app.after_build do |builder|
+      input_path  = "extensions/filelist.txt"
+      output_path = "pdf/terracottas.pdf"
+      puts `prince --input-list=#{input_path} -o #{output_path}`
+    end
+  end
+
+  def after_configuration
+    pdf_proxy(app.data.catalogue)
+  end
+
+  def manipulate_resource_list(resources)
+    pagelist = generate_pagelist
+    baseurl  = "build/"
+    frontmatter, entries, backmatter = sort_contents(resources)
+
+    frontmatter.each { |p| pagelist.puts baseurl + p.destination_path }
+    entries.each     { |p| pagelist.puts baseurl + p.destination_path }
+    backmatter.each  { |p| pagelist.puts baseurl + p.destination_path }
+    pagelist.close
+
+    # return value of this method becomes the new sitemap
+    resources
+  end
+
+  private
+  def generate_pagelist
+    f  = File.new("./extensions/filelist.txt", "w")
+  end
+
+  def pdf_proxy(collection)
+    collection.each do |cat, entry|
+      app.proxy "/print-catalogue/#{cat}.html", options.print_template,
+        :locals => { :entry => entry }, :ignore => true
+    end
+  end
+
+  def sort_contents(resources)
+    pages       = resources.find_all { |p| p.data.sort_order }
+    frontmatter = pages.find_all     { |p| p.data.sort_order < 100 }
+    backmatter  = pages.find_all     { |p| p.data.sort_order >= 100 }
+    entries     = resources.find_all { |p| p.data.catalogue == true }
+
+    entries.select!      { |p| p.destination_path.include? "print-catalogue"}
+    frontmatter.sort_by! { |p| p.data.sort_order }
+    backmatter.sort_by!  { |p| p.data.sort_order }
+    entries.sort_by!     { |p| p.metadata[:locals][:entry][:info][:cat] }
+
+    return frontmatter, entries, backmatter
+  end
+end
+
+::Middleman::Extensions.register(:pdf, PDF)
